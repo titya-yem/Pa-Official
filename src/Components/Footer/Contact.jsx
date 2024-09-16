66dbfd1e04e376c9bc18b6db7ba6e0e600e081b3
Titya <thitya.yem.photo@gmail.com>
2024-09-16 01:27:03 -0400

    Add email to send

--- src/Components/Footer/Contact.jsx
+++ src/Components/Footer/Contact.jsx
@@ -29,42 +29,47 @@ const Contact = () => {
           >
             Contact
           </motion.h1>
-          <div className="flex flex-col gap-6 text-sm lg:text-[16px] sm:w-[60%] lg:w-[70%] sm:mx-auto">
-            <motion.input
-              className="p-6 rounded-md text-black"
-              type="email"
-              name="Email"
-              placeholder="Email"
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={isInView ? { opacity: 1, scale: 1 } : {}}
-              transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
-            />
-            <motion.textarea
-              className="p-6 h-32 rounded-md text-black"
-              type="message"
-              name="Message"
-              placeholder="Message"
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={isInView ? { opacity: 1, scale: 1 } : {}}
-              transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
-            />
-            <motion.button
-              className="p-4 rounded-md text-base lg:text-lg text-black italic font-[500] bg-[#FFD500] hover:bg-transparent hover:text-[#FFD500] hover:border hover:border-solid hover:border-[#FFD500] duration-200"
-              type="submit"
-              initial={{ opacity: 0, scale: 0.8 }} // Button pop-up effect
-              animate={isInView ? { opacity: 1, scale: 1 } : {}}
-              transition={{ duration: 0.8, ease: "easeOut", delay: 0.6 }}
-            >
-              Send Email
-            </motion.button>
-          </div>
+          <form
+            action="https://formsubmit.co/paofficial168@gmail.com"
+            method="POST"
+          >
+            <div className="flex flex-col gap-6 text-sm lg:text-[16px] sm:w-[60%] lg:w-[70%] sm:mx-auto">
+              <motion.input
+                className="p-6 rounded-md text-black"
+                type="email"
+                name="Email"
+                placeholder="Email"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={isInView ? { opacity: 1, scale: 1 } : {}}
+                transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
+              />
+              <motion.textarea
+                className="p-6 h-32 rounded-md text-black"
+                type="message"
+                name="Message"
+                placeholder="Message"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={isInView ? { opacity: 1, scale: 1 } : {}}
+                transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
+              />
+              <motion.button
+                className="p-4 rounded-md text-base lg:text-lg text-black italic font-[500] bg-[#FFD500] hover:bg-transparent hover:text-[#FFD500] hover:border hover:border-solid hover:border-[#FFD500] duration-200"
+                type="submit"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={isInView ? { opacity: 1, scale: 1 } : {}}
+                transition={{ duration: 0.8, ease: "easeOut", delay: 0.6 }}
+              >
+                Send Email
+              </motion.button>
+            </div>
+          </form>
         </motion.div>
         <Email />
       </motion.div>
 
       <motion.p
         className="text-[#FAF5FF] text-sm lg:text-[16px] text-center lg:text-left pb-8 lg:pl-[150px]"
-        initial={{ opacity: 0, scale: 0.8 }} // Pop-up effect for the footer text
+        initial={{ opacity: 0, scale: 0.8 }}
         animate={isInView ? { opacity: 1, scale: 1 } : {}}
         transition={{ duration: 0.8, delay: 0.5, ease: "easeOut" }}
       >
